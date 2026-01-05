16a13fc44b4aec2da76060f7032ec7cd4246d221
yogyy <yogimr261@gmail.com>
2026-01-05 22:48:09 +0700

    feat: add @cloudflare/vite-plugin to fix build

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,18 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: { port: 5173 },
   plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tsConfigPaths({
       projects: ["./tsconfig.json"],
     }),
+    tailwindcss(),
     tanstackStart(),
     viteReact(),
-    tailwindcss(),
   ],
 });
