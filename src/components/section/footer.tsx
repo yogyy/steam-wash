6e4c623c24654e98aa9ec90d885c5e5447001f1d
yogyy <yogimr261@gmail.com>
2026-01-08 00:04:47 +0700

    chore: 2026 🥀

--- src/components/section/footer.tsx
+++ src/components/section/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-3 sm:flex-row md:px-6">
       <p className="text-foreground/80 text-xs">
-        © 2025 Binar-Binar. All rights reserved.
+        © 2026 Binar-Binar. All rights reserved.
       </p>
       <nav className="flex items-center gap-4 sm:ml-auto sm:gap-6">
         <a
