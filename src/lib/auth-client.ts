8c4eb6ae4cfdcd81d6b8713a49f00495f6cec52e
yogyy <yogimr261@gmail.com>
2026-01-07 11:16:38 +0700

    fix: delete tanstack start things

--- src/lib/auth-client.ts
+++
@@ -1,7 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-export const authClient = createAuthClient();
-
-export const { signIn, signUp, useSession } = createAuthClient();
-
-export type Session = typeof authClient.$Infer.Session;
-export type User = typeof authClient.$Infer.Session.user;
