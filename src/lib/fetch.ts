df12660990d18883d3c75e6d0fad09715974d8e5
yogyy <yogimr261@gmail.com>
2026-01-07 12:09:19 +0700

    refactor: simplify fetch logic and remove unused song handling code

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,38 +1,12 @@
-import { createServerFn } from "@tanstack/react-start";
-import z4 from "zod/v4";
-import { playlistRecommendations } from "./db/schema";
-import { db } from "./db";
-import { nanoid } from "nanoid";
-import { setResponseStatus } from "@tanstack/react-start/server";
+interface ResponseTotal {
+  id: string;
+  total: number;
+}
 
-export const getTotalCustomer = createServerFn({ method: "GET" }).handler(
-  async () => await db.query.customer.findFirst(),
-);
+export const localURL = "http://127.0.0.1:8787";
+export const prodURL = "https://steam-be.yogyy.workers.dev";
 
-const newSongSchema = z4.object({
-  userId: z4.string(),
-  songTitle: z4.string(),
-  songLink: z4.string().optional(),
-});
-
-export const addSong = createServerFn({
-  method: "POST",
-})
-  .inputValidator((song: unknown) => newSongSchema.parse(song))
-  .handler(async ({ data }) => {
-    try {
-      const [playlist] = await db
-        .insert(playlistRecommendations)
-        .values({
-          id: nanoid(),
-          ...data,
-        })
-        .returning();
-      setResponseStatus(201);
-      return { playlist };
-    } catch (err) {
-      console.error(err);
-      setResponseStatus(500);
-      return { error: "Failed to create playlist" };
-    }
-  });
+export const fetchTotalCustomer = async (): Promise<ResponseTotal> => {
+  const response = await fetch(`${prodURL}/api/total`);
+  return await response.json();
+};
